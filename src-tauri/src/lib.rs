16ed02d317cd57296732def79c6f79f3a770318b
saleem malik <saleemalik13@gmail.com>
2025-11-28 19:07:27 +0530

    saving future use

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,6 +1,8 @@
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
   tauri::Builder::default()
+    .plugin(tauri_plugin_fs::init())
+    
     .setup(|app| {
       if cfg!(debug_assertions) {
         app.handle().plugin(
@@ -14,3 +16,4 @@ pub fn run() {
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
+
