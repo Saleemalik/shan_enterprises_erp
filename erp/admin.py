7aaeef81db7d116dd6de69bfcc8a958344c7d590
saleem malik <saleemalik13@gmail.com>
2025-12-27 18:56:10 +0530

    admin ready

--- erp/admin.py
+++ erp/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import (
+    ServiceBill,
+    TransportDepotSection,
+    TransportFOLSection,
+    TransportFOLSlab,
+    DestinationEntry,
+)
 
 # Register your models here.
+admin.site.register(ServiceBill)
+admin.site.register(TransportDepotSection)
+admin.site.register(TransportFOLSection)
+admin.site.register(TransportFOLSlab)
+admin.site.register(DestinationEntry)
